95240227b49f59ba140e161590879c08919224df
Asep Saepul Milah <146751939+asepsaepulm@users.noreply.github.com>
2025-02-24 13:17:21 +0700

    Add files via upload

---
+++ src/pertemuan_3/Node.java
@@ -0,0 +1,11 @@
+package pertemuan_3;
+
+public class Node {
+    private int data;
+    private Node next;
+    /** Inisialisasi atribut node */
+    public Node (int data) {
+        this.data = data;
+    }
+/** Setter & Getter */
+}
